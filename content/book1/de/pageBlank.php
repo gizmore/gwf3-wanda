00b7958030de1c1159219d032e915b6995bd9989
SpaceOne <space@wechall.net>
2018-11-27 21:31:36 +0100

    Initial commit: Add Wanda module from gwf3

---
+++ content/book1/de/pageBlank.php
@@ -0,0 +1,11 @@
+<p>
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 1, 1); ?>
+<?php echo $tVars['module']->wandaImage(1, 1, 2); ?>
+<?php echo $tVars['module']->wandaImage(1, 1, 3); ?>
+
+<p>
+</p>
+
+«»
